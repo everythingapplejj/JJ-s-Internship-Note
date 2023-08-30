5cc9f83df6ad64c916838d357923df25f2e4ff38
jiwoong <jiwoong3@illinois.edu>
2023-08-30 02:53:40 +0000

    completed PNG.h

--- CS225/cs225docker/cs225git/mp_stickers/src/Image.cpp
+++ CS225/cs225docker/cs225git/mp_stickers/src/Image.cpp
@@ -1,2 +1,17 @@
 #include "Image.h"
 
+void cs225::Image::Darken() {
+    unsigned int width = PNG::width();
+    unsigned int height = PNG::height();
+    /*
+    iterate through every pixel -> any function?    
+    then use the setter function to decrement the darkness
+    HSLAPixel ^
+    */
+   for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+        
+    }
+   }
+}
+
